98765346d5fb11e852fce08b4a634ad0c9ae00f1
gssbthir <31626165+gssbthir@users.noreply.github.com>
2017-11-15 11:04:17 +0530

    upload client source filkes

---
+++ client/states.js
@@ -0,0 +1,46 @@
+angular.module('app').config(function ($stateProvider) {
+    'use strict';
+    $stateProvider.state('app', {
+        abstract: true,
+        controller: 'app',
+        templateProvider: function (app) {
+            return app.templateProvider('app');
+        }
+    }).state('app.menu', {
+        views: {
+            app: {
+                controller: 'app_menu',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.menu');
+                }
+            }
+        }
+    }).state('app.multiselect', {
+        views: {
+            app: {
+                controller: 'app_multiselect',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.multiselect');
+                }
+            }
+        }
+    }).state('app.result', {
+        views: {
+            app: {
+                controller: 'app_result',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.result');
+                }
+            }
+        }
+    }).state('app.details', {
+        views: {
+            app: {
+                controller: 'app_details',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.details');
+                }
+            }
+        }
+    });
+});
