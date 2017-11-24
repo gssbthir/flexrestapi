36358c6c8578405cfa53bcc7c29e0f6b52557d82
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-11-23 23:08:15 -0800

    [Studio] Modified screen "details"

--- client/controllers/app/details.js
+++ client/controllers/app/details.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('app_details', app_details);
 function app_details($scope, app) {
     'use strict';
-    console.log("inside details controller" +$scope.data);
+    console.log("inside details controller" +sitedetails);
     
     app.init($scope);
 }
