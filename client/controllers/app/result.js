f410289abb320bab55c9f6b1ace66915b1511f7f
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-12-08 09:51:03 -0800

    [Studio] Modified screen "result"

--- client/controllers/app/result.js
+++ client/controllers/app/result.js
@@ -2,5 +2,5 @@ angular.module('app').controller('app_result', app_result);
 function app_result($scope, app) {
     'use strict';
     app.init($scope);
-    console.log($scope.data.app_result);
+    console.log($scope.data.list);
 }
